7188a158b1c792ffb0f6cb6bbee733c96e589d95
francesta <huntleyfrancesta@gmail.com>
2021-04-01 17:27:16 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const testVar = {}
 
+function testFunc() {
+    return "hi"
+}
+
+superbowlWin = (array) => {
+    let result = array.find(elem => elem.result === 'W');
+    if (result) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
