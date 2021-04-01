ffcf87a126f9dd709b585696516b7021c4e37b4e
francesta <huntleyfrancesta@gmail.com>
2021-04-01 17:25:53 -0400

    commit tree 203a7f698f7a03645c68f20395defd4e4fdfad0d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const testVar = {}
 
+function testFunc() {
+    return "hi"
+}
+superbowlWin = (array) => {
+    let results = array.find(elem => elem.result === 'W');
+    if (result) {
+        return result.year;
+    } else {
+        return underfined;
+    }
+}
